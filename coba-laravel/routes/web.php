80d944ec73a10bfbbc7b577aa56aff1b40e4b51a
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-10-31 10:53:13 +0700

    menambahkan catatan laravel di youtube #3

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Lisa BlackPink",
+        "email" => "lisa@unpas.ac.id",
+        "image" => "lisa.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
