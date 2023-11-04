1ce7e74877ebf457e5b1adffd72ea7ac1af31535
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-11-04 17:49:42 +0700

    menambahkan catatan laravel di youtube #10

--- coba-laravel/database/seeders/DatabaseSeeder.php
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -16,19 +16,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
 
-        User::create([
-            'name' => 'Sandhika Galih',
-            'email' => 'sandhika@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Sandhika Galih',
+        //     'email' => 'sandhika@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => 'Doddy Ferdiansyah',
-            'email' => 'doddy@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Doddy Ferdiansyah',
+        //     'email' => 'doddy@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -40,40 +42,42 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab distinctio natus quibusdam ad, minus error, nisi soluta aperiam quos nobis nam nulla dicta assumenda incidunt illo cumque, nihil eaque maiores enim nesciunt atque provident explicabo qui. Illum possimus ex debitis consectetur voluptatum! Quaerat sit blanditiis amet. Doloribus similique dolore deserunt iusto, error quis voluptatem nostrum cum impedit reiciendis alias excepturi, fuga aliquam provident quisquam dolor iste officiis veritatis illo animi repellat asperiores quasi ut! Eum obcaecati, id minus assumenda consectetur nam quis ex nemo praesentium ducimus totam quam aut quibusdam magni soluta saepe? Exercitationem esse distinctio optio tempore adipisci omnis.',
-            'category_id' =>1,
-            'user_id' =>1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab distinctio natus quibusdam ad, minus error, nisi soluta aperiam quos nobis nam nulla dicta assumenda incidunt illo cumque, nihil eaque maiores enim nesciunt atque provident explicabo qui. Illum possimus ex debitis consectetur voluptatum! Quaerat sit blanditiis amet. Doloribus similique dolore deserunt iusto, error quis voluptatem nostrum cum impedit reiciendis alias excepturi, fuga aliquam provident quisquam dolor iste officiis veritatis illo animi repellat asperiores quasi ut! Eum obcaecati, id minus assumenda consectetur nam quis ex nemo praesentium ducimus totam quam aut quibusdam magni soluta saepe? Exercitationem esse distinctio optio tempore adipisci omnis.',
-            'category_id' =>1,
-            'user_id' =>1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab distinctio natus quibusdam ad, minus error, nisi soluta aperiam quos nobis nam nulla dicta assumenda incidunt illo cumque, nihil eaque maiores enim nesciunt atque provident explicabo qui. Illum possimus ex debitis consectetur voluptatum! Quaerat sit blanditiis amet. Doloribus similique dolore deserunt iusto, error quis voluptatem nostrum cum impedit reiciendis alias excepturi, fuga aliquam provident quisquam dolor iste officiis veritatis illo animi repellat asperiores quasi ut! Eum obcaecati, id minus assumenda consectetur nam quis ex nemo praesentium ducimus totam quam aut quibusdam magni soluta saepe? Exercitationem esse distinctio optio tempore adipisci omnis.',
+        //     'category_id' =>1,
+        //     'user_id' =>1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab distinctio natus quibusdam ad, minus error, nisi soluta aperiam quos nobis nam nulla dicta assumenda incidunt illo cumque, nihil eaque maiores enim nesciunt atque provident explicabo qui. Illum possimus ex debitis consectetur voluptatum! Quaerat sit blanditiis amet. Doloribus similique dolore deserunt iusto, error quis voluptatem nostrum cum impedit reiciendis alias excepturi, fuga aliquam provident quisquam dolor iste officiis veritatis illo animi repellat asperiores quasi ut! Eum obcaecati, id minus assumenda consectetur nam quis ex nemo praesentium ducimus totam quam aut quibusdam magni soluta saepe? Exercitationem esse distinctio optio tempore adipisci omnis.',
-            'category_id' =>2,
-            'user_id' =>1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab distinctio natus quibusdam ad, minus error, nisi soluta aperiam quos nobis nam nulla dicta assumenda incidunt illo cumque, nihil eaque maiores enim nesciunt atque provident explicabo qui. Illum possimus ex debitis consectetur voluptatum! Quaerat sit blanditiis amet. Doloribus similique dolore deserunt iusto, error quis voluptatem nostrum cum impedit reiciendis alias excepturi, fuga aliquam provident quisquam dolor iste officiis veritatis illo animi repellat asperiores quasi ut! Eum obcaecati, id minus assumenda consectetur nam quis ex nemo praesentium ducimus totam quam aut quibusdam magni soluta saepe? Exercitationem esse distinctio optio tempore adipisci omnis.',
+        //     'category_id' =>1,
+        //     'user_id' =>1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab distinctio natus quibusdam ad, minus error, nisi soluta aperiam quos nobis nam nulla dicta assumenda incidunt illo cumque, nihil eaque maiores enim nesciunt atque provident explicabo qui. Illum possimus ex debitis consectetur voluptatum! Quaerat sit blanditiis amet. Doloribus similique dolore deserunt iusto, error quis voluptatem nostrum cum impedit reiciendis alias excepturi, fuga aliquam provident quisquam dolor iste officiis veritatis illo animi repellat asperiores quasi ut! Eum obcaecati, id minus assumenda consectetur nam quis ex nemo praesentium ducimus totam quam aut quibusdam magni soluta saepe? Exercitationem esse distinctio optio tempore adipisci omnis.',
-            'category_id' =>2,
-            'user_id' =>2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab distinctio natus quibusdam ad, minus error, nisi soluta aperiam quos nobis nam nulla dicta assumenda incidunt illo cumque, nihil eaque maiores enim nesciunt atque provident explicabo qui. Illum possimus ex debitis consectetur voluptatum! Quaerat sit blanditiis amet. Doloribus similique dolore deserunt iusto, error quis voluptatem nostrum cum impedit reiciendis alias excepturi, fuga aliquam provident quisquam dolor iste officiis veritatis illo animi repellat asperiores quasi ut! Eum obcaecati, id minus assumenda consectetur nam quis ex nemo praesentium ducimus totam quam aut quibusdam magni soluta saepe? Exercitationem esse distinctio optio tempore adipisci omnis.',
+        //     'category_id' =>2,
+        //     'user_id' =>1
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit.',
+        //     'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab distinctio natus quibusdam ad, minus error, nisi soluta aperiam quos nobis nam nulla dicta assumenda incidunt illo cumque, nihil eaque maiores enim nesciunt atque provident explicabo qui. Illum possimus ex debitis consectetur voluptatum! Quaerat sit blanditiis amet. Doloribus similique dolore deserunt iusto, error quis voluptatem nostrum cum impedit reiciendis alias excepturi, fuga aliquam provident quisquam dolor iste officiis veritatis illo animi repellat asperiores quasi ut! Eum obcaecati, id minus assumenda consectetur nam quis ex nemo praesentium ducimus totam quam aut quibusdam magni soluta saepe? Exercitationem esse distinctio optio tempore adipisci omnis.',
+        //     'category_id' =>2,
+        //     'user_id' =>2
+        // ]);
     }
 }
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,5 +47,12 @@ Route::get('/categories/{category:slug}', function(Category $category){
         'title' => $category->name,
         'posts' => $category->posts,
         'category' => $category->name
+    ]);
+});
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => 'User Posts',
+        'posts' => $author->posts
     ]);
 });
