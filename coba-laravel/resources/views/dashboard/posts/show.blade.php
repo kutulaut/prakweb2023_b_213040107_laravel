09366ba7fd706b76855d73e3adab706acd34c3a7
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-11-26 11:45:47 +0700

    menambahkan catatan laravel di youtube #20

--- coba-laravel/resources/views/dashboard/posts/show.blade.php
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -9,10 +9,13 @@
 
          <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span>
           Back to all my posts</a>
-         <a href="" class="btn btn-warning"><span data-feather="edit"></span>
+         <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span>
           Edit</a>
-         <a href="" class="btn btn-danger"><span data-feather="x-circle"></span>
-          Delete</a>
+        <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+          @method('delete')
+          @csrf
+          <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span>Delete</button>
+        </form>
 
         <img src="https://source.unsplash.com/1200x400?{{$post->category->name }}"
          alt="{{$post->category->name }}" class="img-fluid mt-3">
@@ -20,8 +23,6 @@
          <article class="my-3 fs-7">
           {!!$post->body!!}
          </article>
-
-        <a href="/posts" class="d-block mt-3">Back to posts</a>
           </div>
       </div>
   </div>
