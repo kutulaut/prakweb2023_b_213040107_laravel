f5600323697bccfc8ee454fa6e06945c5c5b75ae
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-11-26 20:22:16 +0700

    menambahkan catatan laravel di youtube #22

--- coba-laravel/resources/views/dashboard/posts/create.blade.php
+++ coba-laravel/resources/views/dashboard/posts/create.blade.php
@@ -43,8 +43,9 @@
     </div>
     <div class="mb-3">
         <label for="image" class="form-label">Post Image</label>
+        <img class="img-preview img-fluid mb-3 col-sm-5">
         <input class="form-control @error('image') is-invalid @enderror" type="file" id="image"
-        name="image">
+        name="image" onchange="previewImage()">
         @error('image')
           <div class="invalid-feedback">
             {{ $message }}
@@ -63,6 +64,8 @@
   </form>
 </div>
 
+
+
 <script>
   const title = document.querySelector('#title');
   const slug = document.querySelector('#slug');
@@ -76,5 +79,16 @@
   document.addEventListener('trix-file-accept', function(e) {
         e.preventDefault();
     });
+
+    function previewImage() {
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+        imgPreview.style.display = 'block';
+        const ofReader = new FileReader();
+        ofReader.readAsDataURL(image.files[0]);
+        ofReader.onload = function(oFREvent) {
+            imgPreview.src = oFREvent.target.result;
+        }
+    }
 </script>
 @endsection
