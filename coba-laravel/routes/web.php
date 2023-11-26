1260d1cf46ca8b7c953f3543838f1be1ba6b1ddf
Dicky Rahmanto <dickyrahmantojr@gmail.com>
2023-11-26 21:06:08 +0700

    menambahkan catatan laravel di youtube #23

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -80,3 +81,6 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'check
 
 Route::resource('/dashboard/posts', DashboardPostController::class)->
 middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')
+->middleware('admin');
